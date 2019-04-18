24339ce96b5f512cc2f1be49d9a0828016881e07
Carlos Baron <carlostectol@gmail.com>
2019-04-18 11:58:20 -0500

    Nothing important, it still works

--- App/src/app/header/header.component.ts
+++ App/src/app/header/header.component.ts
@@ -11,7 +11,7 @@ import { AuthService } from '../auth/auth.service';
 })
 export class HeaderComponent {
   constructor(private dataStorageService: DataStorageService,
-    private authService: AuthService) {
+              private authService: AuthService) {
 
   }
   onSaveData() {
