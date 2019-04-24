0c39ea924610c037f1d82340d8a7f54bf53483d0
Carlos Baron <carlostectol@gmail.com>
2019-04-24 15:02:05 -0500

    New DB and status
    New we are using a new database and giving the status to the tractor

--- App/src/app/shared/ingredient.module.ts
+++ App/src/app/shared/ingredient.module.ts
@@ -1,4 +1,9 @@
 export class Caracteristica {
-  constructor(public name: string, public amount: number){
+  constructor(public name: string, public amount: number) {
+  }
+}
+
+export class NameTractor {
+  constructor(public nameTractor: string, public status: string) {
   }
 }
