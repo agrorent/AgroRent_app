56f3805b7f55bbfefc598df48f1deee484b5a933
Carlos Baron <carlostectol@gmail.com>
2019-04-29 10:52:42 -0500

    dfgdf

--- App/src/app/recipes/recipe.model.ts
+++ App/src/app/recipes/recipe.model.ts
@@ -7,12 +7,21 @@ export class Tractor {
   public caracteristicas: Caracteristica[];
   public status: string
 
+<<<<<<< HEAD
   constructor(names: string, desc: string, imaPath: string, caracteristicas: Caracteristica[], status: string ) {
     this.name = names;
     this.description = desc;
     this.imagePath = imaPath;
     this.caracteristicas = caracteristicas;
     this.status = status;
+=======
+  constructor(names: string, desc: string, imaPath: string, ingredients: Caracteristica[], statuses: string ) {
+    this.name = names;
+    this.description = desc;
+    this.imagePath = imaPath;
+    this.caracteristicas = ingredients;
+    this.status = statuses;
+>>>>>>> parent of d3ffff81... Works apartado
   }
 
 }
