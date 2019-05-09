4feb5d35bde146a027dec0389b4cba87d53a2e64
Carlos Baron <carlostectol@gmail.com>
2019-05-09 09:39:18 -0500

    Now is working

--- App/src/app/shopping-list/shopping-list.component.ts
+++ App/src/app/shopping-list/shopping-list.component.ts
@@ -8,29 +8,15 @@ import { ApartadoListService } from './shopping-list.service';
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
 })
-export class ApartadoComponent implements OnInit, OnDestroy {
+export class ApartadoComponent implements OnInit {
   caracteristicas: Caracteristica[];
   apartados: Apartado[];
-  private subscription: Subscription;
 
   constructor(private slService: ApartadoListService) {
 
   }
   ngOnInit() {
-    this.caracteristicas = this.slService.getCaracteristicas();
-    this.subscription = this.slService.caracteristicasChanged
-     .subscribe(
-      (caracteristicas: Caracteristica[]) => {
-        this.caracteristicas = caracteristicas;
-      }
-      );
 
     this.apartados = this.slService.getApartados();
-  }
-  onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
-  }
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 }
