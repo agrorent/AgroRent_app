aa7916e12282a5ac8db05f47a78f7adae74d1a98
Benjamin <benjaavro@gmail.com>
2019-05-17 02:39:40 -0500

    back to original model

--- App/src/app/recipes/recipe.model.ts
+++ App/src/app/recipes/recipe.model.ts
@@ -6,27 +6,13 @@ export class Tractor {
   public imagePath: string;
   public caracteristicas: Caracteristica[];
   public status: string;
-  public precio: number;
-  public tipo: string;
-  public power: string;
-  public marca: string;
-  public granos: string;
-  public numDiscos: number;
-  public mecanismo: string;
 
-  constructor(names: string, desc: string, imaPath: string, caracteristicas: Caracteristica[], statuses: string, precio: number, tipo: string, power: string, marca: string, granos: string, numDiscos: number, mecanismo: string){
+  constructor(names: string, desc: string, imaPath: string, caracteristicas: Caracteristica[], statuses: string){
     this.name = names;
     this.description = desc;
     this.imagePath = imaPath;
     this.caracteristicas = caracteristicas;
     this.status = statuses;
-    this.precio = precio;
-    this.tipo = tipo;
-    this.power = power;
-    this.marca = marca;
-    this.granos = granos;
-    this.numDiscos = numDiscos;
-    this.mecanismo = mecanismo;
   }
 
 }
